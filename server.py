69e913b2f70c7aea6f2cf92eedd63e0beccab121
Alissa Sobo <alissasobo@gmail.com>
2016-12-28 13:30:49 -0800

    add database setup and form for a new item

--- server.py
+++ server.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
+from database_setup import Base, Item
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 def FrontPage():
     return render_template('front_page.html')
+
+@app.route('/item/new', methods=['GET', 'POST'])
+def newItem():
+    if request.method == 'GET':
+        return render_template('new_item.html')
+    if request.method == 'POST':
+        newItem = Item()
 
 
 
