b0efe25970eb62af69a36d0b53f5f003585fb69a
Alissa Sobo <alissasobo@gmail.com>
2017-03-21 14:15:38 -0700

    change postgres url

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ import requests
 
 #Connect to Database and create database session
 #'postgresql+psycopg2://user:password@hostname/database_name'
-engine = create_engine('postgresql+psycopg2://catalog:@127.0.0.1:5432/catalog')
+engine = create_engine('postgresql+psycopg2://catalog:catalog@127.0.0.1:5432/catalog')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
